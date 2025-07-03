e53fb7c45e5f6d055223d7c92aee495c45fe395b
Andypandita04 <andreafigueroa04@mail.com>
2025-07-03 13:31:10 -0600

    cors en app.js

--- src/app.js
+++ src/app.js
@@ -1,29 +1,33 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import proyectoRoutes from './routes/proyectoRoutes.js';
 import errorHandler from './middlewares/errorHandler.js';
-
-
+ 
+ 
+ 
+ 
 // Configurar dotenv
 dotenv.config();
-
+ 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+ 
+app.use(cors())
 // Middleware para parsear JSON
 app.use(express.json());
 
 // Rutas
 app.use('/proyectos', proyectoRoutes);
-
+ 
 // Ruta básica de prueba
 app.get('/', (req, res) => {
   res.send('API funcionando');
 });
-
+ 
 // Manejo de errores
 app.use(errorHandler);
-
+ 
 // Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
