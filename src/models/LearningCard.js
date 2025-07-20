d9014cd1147b87c5abc5486f81a9bac81895b943
Andypandita04 <andreafigueroa04@gmail.com>
2025-07-19 22:23:06 -0600

    Arreglo id_learning card response get/patch/post

--- src/models/LearningCard.js
+++ src/models/LearningCard.js
@@ -8,12 +8,11 @@ import ApiError from '../utils/ApiError.js';
 class LearningCard {
   constructor(data = {}) { // Valor por defecto para data
     // Asignamos valores por defecto para todas las propiedades
-    this.id = data.id || null;
+    this.id_learning_card = data.id || null;
     this.id_secuencia = data.id_secuencia || null;
     this.id_testing_card = data.id_testing_card || null;
     this.resultado = data.resultado || null;
     this.hallazgo = data.hallazgo || null;
-    this.estado = data.estado || 'CUMPLIDO'; // Nuevo atributo
     this.created_at = data.created_at ? new Date(data.created_at) : new Date();
     this.updated_at = data.updated_at ? new Date(data.updated_at) : new Date();
   }
@@ -50,11 +49,10 @@ class LearningCard {
 
   toAPI() {
     return {
-      id: this.id,
+      id_learning_card: this.id_learning_card,
       id_testing_card: this.id_testing_card,
       resultado: this.resultado,
       hallazgo: this.hallazgo,
-      estado: this.estado, // Nuevo atributo
       creado: this.created_at.toISOString(),
       actualizado: this.updated_at.toISOString()
     };
